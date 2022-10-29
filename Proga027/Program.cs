e01f52754700a9b9352766b26e8a37a6b0dd444d
ElenaZateeva <zateevaelena14@gmail.com>
2022-10-30 00:07:21 +0300

    Сумма цифр заданного числа

---
+++ Proga027/Program.cs
@@ -0,0 +1,31 @@
+﻿// На входе число. На выходе сумма цифр данного числа
+Console.WriteLine("Введите любое число:");
+bool isParsedA = int.TryParse(Console.ReadLine(), out int a);
+
+if (!isParsedA)
+{
+    Console.WriteLine("ОШИБКА ввода данных!");
+    return;
+}
+
+if (a < 0)
+{
+    Console.WriteLine("0");
+    return;
+}
+
+int summa = SumOfDigits(a);
+Console.WriteLine($"Сумма цифр из заданного числа: {summa}");
+
+int SumOfDigits(int a)
+{
+    int sum = 0, ost = 0;
+    while (a > 0)
+    {
+        ost = a % 10;
+        a = a / 10;
+        sum = sum + ost;
+    }
+    return sum;
+}
+
