059044d84c7148fc7e0f55e391704e6598be0e94
ElenaZateeva <zateevaelena14@gmail.com>
2022-10-30 00:46:13 +0300

    Ввод массива из 8 элементов и печать в скобках

---
+++ Proga029/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задает массив и выводит его на экран в [] скобках
+Console.WriteLine("Введите массив чисел из 8 элементов");
+int[] array = new int[8];
+VvodArray(array);
+PrintArray(array);
+
+//Ввод элементов массива с консоли
+void VvodArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = Convert.ToInt32(Console.ReadLine());
+    }
+}
+
+void PrintArray(int[] array)
+{
+    Console.Write("[ ");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine("]");
+}
